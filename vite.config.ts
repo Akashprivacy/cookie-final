84b7bb29393ce072699c054f88022bb907559bf0
Akashprivacy <akashchoudhury90640@gmail.com>
2025-08-18 17:38:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,21 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, '.', '');
+  return {
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    },
+    server: {
+      allowedHosts: [
+        'cookie-final-768840799099.asia-south1.run.app',
+      ]
+    }
+  };
 });
