221c48dfda8c1dc4a34b764542b16a3797a2c52a
Akashprivacy <akashchoudhury90640@gmail.com>
2025-09-09 18:55:28 +0530

    Update types.ts

--- types.ts
+++ types.ts
@@ -49,6 +49,8 @@ export interface TrackerInfo {
     complianceStatus: ComplianceStatus;
     remediation: string;
     pagesFound: string[];
+    databaseClassification?: string;
+    oneTrustClassification?: string;
 }
 
 export interface LocalStorageInfo {
